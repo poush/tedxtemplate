4af5d26966e17785b5368f6963ce3ca369b16839
Piyush Agrawal <poush12@gmail.com>
2016-09-25 19:08:05 +0530

    small fixes

--- core/js/app.js
+++ core/js/app.js
@@ -3,8 +3,24 @@
 
 $(window).scroll(function(){
 	var height = $(window).scrollTop();
-	if(height > 150)
+
+	if(height > 50)
 		$('nav').addClass('shrink');
 	else
 		$('nav').removeClass('shrink');
+
+})
+
+$("nav ul li a[href^='#']").on('click', function(e){
+
+	e.preventDefault();
+
+	var hash = this.hash;
+
+	$('html,body').animate({
+		scrollTop: $(hash).offset().top
+	}, 1500, function(){
+		window.location.hash = hash;
+	})
+
 })
