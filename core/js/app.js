ce5782687f4494fadb183c317db649b83ce59448
Piyush Agrawal <poush12@gmail.com>
2016-09-25 22:40:32 +0530

    mobile menu changes

--- core/js/app.js
+++ core/js/app.js
@@ -1,5 +1,10 @@
 // jQuery Code
 
+$(function(){
+	var height = $(window).scrollTop();
+	if(height > 50)
+		$('nav').addClass('shrink');
+})
 
 $(window).scroll(function(){
 	var height = $(window).scrollTop();
@@ -19,8 +24,14 @@ $("nav ul li a[href^='#']").on('click', function(e){
 
 	$('html,body').animate({
 		scrollTop: $(hash).offset().top
-	}, 1500, function(){
+	}, 500, function(){
 		window.location.hash = hash;
 	})
 
 })
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
