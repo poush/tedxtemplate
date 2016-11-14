a9a4279287c7f616edd512ca7cbfde63cbc87520
shashwat2519 <shashwat.gulyani@gmail.com>
2016-11-14 12:22:33 +0530

    owl-carousel added, json file added for team-data

--- core/js/app.js
+++ core/js/app.js
@@ -34,4 +34,5 @@ $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
+
 });
